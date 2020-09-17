df88167841f11ce125dcb6c46886075028b19774
margo10011990 <margo10011990@gmail.com>
2020-09-17 16:52:19 +0300

    Updates

---
+++ precache-manifest.751b9a4d1171fd9769dcab3849037485.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c696db24c5e36cba065162379e194e6",
+    "url": "/discover-mars/index.html"
+  },
+  {
+    "revision": "35fc74a57ea1c9e3b6f8",
+    "url": "/discover-mars/static/css/2.f3ad4bda.chunk.css"
+  },
+  {
+    "revision": "ce269f023190318f0cbe",
+    "url": "/discover-mars/static/css/main.7e5446a5.chunk.css"
+  },
+  {
+    "revision": "35fc74a57ea1c9e3b6f8",
+    "url": "/discover-mars/static/js/2.f4945a71.chunk.js"
+  },
+  {
+    "revision": "ebd4d314a8e8bb916caf04b45f7209b2",
+    "url": "/discover-mars/static/js/2.f4945a71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce269f023190318f0cbe",
+    "url": "/discover-mars/static/js/main.f4627e5a.chunk.js"
+  },
+  {
+    "revision": "e676134223039d0fe7e8",
+    "url": "/discover-mars/static/js/runtime-main.3f65739e.js"
+  }
+]);
